fdf9dec62969da9e3ddaebbcd4d1dae6038883db
Michal Urbanek <urbanekmichal.98@gmail.com>
2022-07-21 18:31:23 +0200

    generate spring boot project

---
+++ src/main/java/com/urbanek/michal/rentsystem/RentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.urbanek.michal.rentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentSystemApplication.class, args);
+	}
+
+}
